db78ae5a00089d222ad4130faee77f8a8a504c31
Shinya Ishikawa <ishikawa@rt-net.jp>
2021-01-18 14:29:34 +0900

    Add pins/i2c

---
+++ mouse2/Core/Inc/pins/i2c.h
@@ -0,0 +1,16 @@
+#ifndef _M5_PINS_I2C_H
+#define _M5_PINS_I2C_H
+#include "stm32f4xx_hal.h"
+
+struct m5I2CConfigurationRecord {
+  I2C_HandleTypeDef *handler;
+  GPIO_TypeDef *cs_port;
+  uint16_t cs_pin;
+};
+typedef struct m5I2CConfigurationRecord m5I2CConfigurationRecord;
+typedef struct m5I2CConfigurationRecord *m5I2CConfiguration;
+
+uint8_t m5i2c_read_byte(m5I2CConfiguration i2c, uint8_t reg, size_t size,);
+void m5i2c_write_byte(m5I2CConfiguration i2c, uint8_t reg, uint8_t val, size_t size);
+
+#endif
---
+++ mouse2/Core/Src/pins/i2c.c
@@ -0,0 +1,9 @@
+#include "pins/i2c.h"
+
+uint8_t m5i2c_read_byte_sync(m5I2CConfiguration i2c, uint8_t reg, size_t size) {
+
+}
+
+void m5i2c_write_byte_sync(m5I2CConfiguration i2c, uint8_t reg, uint8_t val, size_t size) {
+
+}
