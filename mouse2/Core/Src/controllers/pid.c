f3a2ad80fa264981a5528b3afb5d26f7d18167ba
Shinya Ishikawa <ishikawa@rt-net.jp>
2021-01-18 14:30:18 +0900

    Add pid update

--- mouse2/Core/Src/controllers/pid.c
+++ mouse2/Core/Src/controllers/pid.c
@@ -1,7 +1,8 @@
-#define M5DEFAULT_PGAIN 0.1f
-#define M5DEFAULT_IGAIN 0.1f
-#define M5DEFAULT_DGAIN 0.1f
-typedef int m5Value;
+#include <stm32f4xx_hal.h>
+#define M5DEFAULT_PGAIN (20.0f)
+#define M5DEFAULT_IGAIN (0.20f)
+#define M5DEFAULT_DGAIN (0.0f)
+typedef float m5Value;
 
 struct m5PIDGainRecord {
   float P;
@@ -12,7 +13,7 @@ typedef m5PIDGainRecord m5PIDGainRecord;
 typedef m5PIDGainRecord *m5PIDGain;
 
 struct m5PIDContextRecord {
-  int initialized;
+  uint8_t initialized;
   m5Value last_value;
   m5Value integrated_value;
 };
@@ -42,4 +43,20 @@ m5PIDController m5PIDCOntroller() {
     &ctx
   };
   return &record;
+}
+
+m5pid_update(m5PIDController pid, m5Value target_value, m5Value current_value) {
+  if (pid->context->activated) {
+    pid->context->last_value = current_value;
+  }
+  m5Value diff = target_value - current_value;
+
+  // update_context
+  m5Value integrated = pid->context->integrated_value =
+      pid->context->integrated_value += diff;
+  m5Value last_value = pid->context->last_value;
+  float p = diff * pid->gain->P;
+  float i = integrated * pid->gain->I;
+  float d = (current_value - last_value) * pid->gain->D;
+  pid->value = p + i + d;
 }
